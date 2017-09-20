cd7d80a9b028e38c232eb23877e4712a62019a3c
funador <jesse.heaslip@gmail.com>
2017-09-19 21:52:20 -0500

    updating text changings in tests

--- test/test-reading-level.js
+++ test/test-reading-level.js
@@ -5,7 +5,6 @@ const text1 = 'this, is a simple sentence'
 const text2 = 'the perpendicular platypus perused the panoramic pyramid'
 const text3 = ''
 const text4 = '0120131908 7412398741982374192847382947198374139847'
-const text6 = '1 2 4 6'
 
 describe('readingLevel', () => {
   
@@ -23,20 +22,20 @@ describe('readingLevel', () => {
 
   describe('empty string', () => {
     it('should return error message on empty string', () => {
-      assert.equal('Either no sentences or words, please enter some text', readingLevel(text3))
+      assert.equal('Either no sentences or words, please enter valid text', readingLevel(text3))
     })
   })
 
   describe('numeric values', () => {
     it('should return error message on string with all numeric values', () => {
-      assert.equal('Either no sentences or words, please enter some text', readingLevel(text4))
+      assert.equal('Either no sentences or words, please enter valid text', readingLevel(text4))
     })
   })
 
   describe('full return values', () => {
     it('should return an object with all the values', () => {
       const result = readingLevel(text2, 'full')
-      console.log(result)
+      
       assert.equal(1, result.sentences)
       assert.equal(7, result.words)
       assert.equal(20, result.syllables)
@@ -49,12 +48,13 @@ describe('readingLevel', () => {
   describe('full return values with error', () => {
     it('should return an object with all the values', () => {
       const result = readingLevel(text3, 'full')
+
       assert.equal(0, result.sentences)
       assert.equal(0, result.words)
       assert.equal(0, result.syllables)
       assert.equal(isNaN(NaN), isNaN(result.full))
       assert.equal(isNaN(NaN), isNaN(result.rounded))
-      assert.equal('Either no sentences or words, please enter some text', result.error)
+      assert.equal('Either no sentences or words, please enter valid text', result.error)
     })
   })
 
