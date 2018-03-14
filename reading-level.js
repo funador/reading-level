08c78804fa870633c9433edf27123776e168a0a1
Darkenetor <darkenetor@gmail.com>
2018-03-14 04:23:47 +0100

    linear time complexity from near quadratic for sentence tokenization; improved sentences delimiting behavior according to 5b of https://www.tdi.texas.gov/pubs/pc/pccpfaq.html

--- reading-level.js
+++ reading-level.js
@@ -1,5 +1,4 @@
 const syllable = require('syllable')
-const Tokenizer = require('sentence-tokenizer')
 
 exports.readingLevel = (text, full) => {
   const err = 'Either no sentences or words, please enter valid text'
@@ -16,11 +15,12 @@ exports.readingLevel = (text, full) => {
     return err
   }
 
-  const tokenizer = new Tokenizer('ChuckNorris')
-  tokenizer.setEntry(text)
-  
-  const tokenSentences = tokenizer.getSentences()
-             
+  const tokenSentences = text
+    .replace('\0', '')
+	.replace(/(mr|mrs|dr|ms|prof|rev|col|cmdr|flt|lt|brgdr|hon|wng|capt|rt|revd|gen|cdre|admrl|herr|hr|frau|alderman|alhaji|brig|cdr|cik|consul|datin|dato|datuk|seri|dhr|dipl|ing|dott|sa|dra|drs|en|encik|eng|eur|exma|sra|exmo|sr|lieut|fr|fraulein|fru|graaf|gravin|grp|hajah|haji|hajim|hra|ir|lcda|lic|maj|mlle|mme|mstr|nti|sri|rva|sig|na|ra|sqn|ldr|srta|wg)\./gi, '$1')
+	.replace(/(((^|\w).*?[^\w\s,]+)(?=\s+[A-Z])|:|;)/g, '$1\0')
+    .split(/\s*\0\s*/)
+
   const tracker = {
     syllables: 0,
     words: 0
